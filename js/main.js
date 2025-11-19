737b945df27e752a243bb4b2db1ef26935d423fd
hugooohm <hugo.hernansanz@alumnos.udit.es>
2025-11-19 21:03:03 +0100

    Prueba 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Inicializar AOS (aunque de momento no le hemos puesto data-aos a nada)
+AOS.init({
+  duration: 700,
+  easing: "ease-out-quart",
+  once: true
+});
+
+// Aquí iremos metiendo poco a poco interacciones con jQuery
+$(document).ready(function () {
+  console.log("STRICTO listo 🚀");
+});
